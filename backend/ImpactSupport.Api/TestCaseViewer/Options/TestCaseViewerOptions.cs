ff67c0de152bcd9456de3a9e001838b62f0cf9d7
Yasar <yasar.mohideen@nkw.pub>
2026-08-13 10:47:07 +0530

    feat: add TestCaseViewer configuration and CORS

---
+++ backend/ImpactSupport.Api/TestCaseViewer/Options/TestCaseViewerOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace ImpactSupport.Api.TestCaseViewer.Options
+{
+    public class TestCaseViewerOptions
+    {
+        public string[] DefaultSheetNames { get; set; } =
+        ["Testcase_2026", "Testcase_2025"];
+
+        public int AutoRefreshMinutes { get; set; } = 5;
+    }
+}
